816124179a1731fc79a5cff4fc30614af72e1b36
Youssouf SISSOKO <ysissoko78@gmail.com>
2024-04-08 19:29:04 +0200

    chore: remove unused code and unused parameters

--- src/web3/solana/wallets/legacy.js
+++ src/web3/solana/wallets/legacy.js
@@ -1,22 +1,21 @@
-const logger = require('src/logger')
-const { web3 } = require('src/services/config.service');
-const fs = require('fs');
+const logger = require("src/logger");
+const fs = require("fs");
 
 function loadKeyPair(path) {
-    const keypair_file = fs.readFileSync(path);
-    
-    const secret_key = Buffer.from(JSON.parse(keypair_file.toString()));
-    const my_key_pair = Keypair.fromSecretKey(secret_key);
-    
-    return my_key_pair;
+  const keypair_file = fs.readFileSync(path);
+
+  const secret_key = Buffer.from(JSON.parse(keypair_file.toString()));
+  const my_key_pair = Keypair.fromSecretKey(secret_key);
+
+  return my_key_pair;
 }
 
 /**
  * Create a new wallet
  * @returns a solana wallet
  */
-function createWallet(path) {
-    return Keypair.generate();
+function createWallet() {
+  return Keypair.generate();
 }
 
 /**
@@ -25,12 +24,12 @@ function createWallet(path) {
  * @returns the solana wallet
  */
 function useExistingWallet(path) {
-    try {
-        return loadKeyPair(path);        
-    } catch(e) {
-        logger.warn(`impossible to load your key pair from file. ${e}`);
-        return null;
-    }
+  try {
+    return loadKeyPair(path);
+  } catch (e) {
+    logger.warn(`impossible to load your key pair from file. ${e}`);
+    return null;
+  }
 }
 
-module.exports = { useExistingWallet, createWallet }
+module.exports = { useExistingWallet, createWallet };
