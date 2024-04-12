fdffffd7e11eb661441b637c8f2c6d05803e02b5
Youssouf SISSOKO <ysissoko78@gmail.com>
2024-04-12 20:24:35 +0200

    feat: protect upload route with tokenVerifier middleware

--- src/routes/upload.route.js
+++ src/routes/upload.route.js
@@ -1,10 +1,10 @@
 const upload = require('src/multer/storage');
 const express = require('express');
 const router = express.Router();
-
+const tokenVerifier = require("src/middlewares/token-verifier")
 
 // Mint a new nft
-router.post('', upload.single('file'), function(req, res) {
+router.post('', tokenVerifier(), upload.single('file'), function(req, res) {
     const { file } = req;
     res.send({ file });
 });
